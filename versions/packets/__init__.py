cd57b9bbae27262db5a77f86a96b8c07e1a3c962
Monstrofil <oshyshatskyi@cloudlinux.com>
2017-07-26 14:34:43 +0300

    Fix deprecated imports

--- versions/packets/__init__.py
+++ versions/packets/__init__.py
@@ -4,11 +4,7 @@ from GameLogic import GameLogic
 from Map import Map
 from MemoryData import MemoryData
 from Position import Position
-from Ribbon import Ribbon
-from WorldLogic import WorldLogic
 from Camera import Camera
 from Entity import Entity
-from Time import Time
-from BattleResult import BattleResult
 
 __author__ = "Aleksandr Shyshatsky"
