5562ade763753a7275a46cab03596f7d9beb11e3
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-08-22 22:56:45 +0300

    fixed map packet for wot

--- replay_unpack/packets/Map.py
+++ replay_unpack/packets/Map.py
@@ -9,11 +9,12 @@ from replay_unpack.base.packets.types.Matrix4 import Matrix4
 __author__ = "Aleksandr Shyshatsky"
 
 
-@bigworld_packet(type_=0x27)
+@bigworld_packet(type_=0xf)
 class Map(PacketDataBase):
     def __init__(self, stream):
+        # r = stream.read()
         self.spaceId, = struct.unpack('i', stream.read(4))
-        self.arenaId, = struct.unpack('q', stream.read(8))
-        _name_size, = struct.unpack('i', stream.read(4))
+        self.arenaId, = struct.unpack('i', stream.read(4))
+        _name_size, = struct.unpack('b', stream.read(1))
         self.name = stream.read(_name_size)
         self.matrix = Matrix4(stream)
