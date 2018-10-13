452770586d0385607d427468dc4705df4561569d
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-10-13 20:58:01 +0300

    improve sentry handler, add logging hook

--- replay_unpack/sentry.py
+++ replay_unpack/sentry.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # coding=utf-8
-from raven import Client
+import logging
 
-__author__ = "Aleksandr Shyshatsky"
-client = Client('https://1c55b1f4826242f9a137740d26c50a54:ee2dd20c52934bd48662a80f3d96dc5e@sentry.io/205092')
+from raven import Client, setup_logging
+from raven.handlers.logging import SentryHandler
+
+DSN = 'https://1c55b1f4826242f9a137740d26c50a54:ee2dd20c52934bd48662a80f3d96dc5e@sentry.io/205092'
+
+client = Client(DSN)
+handler = SentryHandler(client)
+handler.setLevel(logging.WARNING)
+setup_logging(handler)
