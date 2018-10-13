81b82e32dcda26d9508aa58554e2cd549ed61932
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-10-13 20:58:23 +0300

    typo

--- replay_unpack/packets/BasePlayerCreate.py
+++ replay_unpack/packets/BasePlayerCreate.py
@@ -12,9 +12,9 @@ __author__ = "Aleksandr Shyshatsky"
 class BasePlayerCreate(PacketDataBase):
     """
     This method is called to create a new player as far as required to
-	talk to the base entity. Only data shared between the base and the
-	client is provided in this method - the cell data will be provided by
-	onCellPlayerCreate later if the player is put on the cell also.
+    talk to the base entity. Only data shared between the base and the
+    client is provided in this method - the cell data will be provided by
+    onCellPlayerCreate later if the player is put on the cell also.
     """
     def __init__(self, stream):
         self.entityId, = struct.unpack('i', stream.read(4))
