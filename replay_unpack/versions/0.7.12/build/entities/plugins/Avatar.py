2d357c341e30222e1616315bbf6bc813d5c169f2
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-12-22 19:29:32 +0200

    0.7.12

---
+++ replay_unpack/versions/0.7.12/build/entities/plugins/Avatar.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding=utf-8
+from build.entities import Avatar
+
+__author__ = "Aleksandr Shyshatsky"
+
+
+class Avatar(Avatar):
+    def __init__(self):
+        super(Avatar, self).__init__()
