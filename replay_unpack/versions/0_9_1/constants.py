8e659a6617269bb122c95ec7c4bf167392b021b8
Monstrofil <shalal545@gmail.com>
2020-02-12 18:54:32 +0200

    Update constants.py

--- replay_unpack/versions/0_9_1/constants.py
+++ replay_unpack/versions/0_9_1/constants.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 __author__ = "Aleksandr Shyshatsky"
 
-id_property_map = {0: 'accountDBID', 1: 'avatarId', 2: 'camouflageInfo', 3: 'clanColor', 4: 'clanID', 5: 'clanTag', 6: 'crewParams', 7: 'dogTag', 8: 'fragsCount', 9: 'friendlyFireEnabled', 10: 'id', 11: 'invitationsEnabled', 12: 'isAbuser', 13: 'isAlive', 14: 'isBot', 15: 'isClientLoaded', 16: 'isConnected', 17: 'isHidden', 18: 'isLeaver', 19: 'isPreBattleOwner', 20: 'isSpectator', 21: 'killedBuildingsCount', 22: 'maxHealth', 23: 'name', 24: 'preBattleIdOnStart', 25: 'preBattleSign', 26: 'prebattleId', 27: 'realm', 28: 'shipComponents', 29: 'shipId', 30: 'shipParamsId', 31: 'skinId', 32: 'teamId', 33: 'ttkStatus'}
+id_property_map = {0: 'accountDBID', 1: 'avatarId', 2: 'camouflageInfo', 3: 'clanColor', 4: 'clanID', 5: 'clanTag', 6: 'crewParams', 7: 'dogTag', 8: 'fragsCount', 9: 'friendlyFireEnabled', 10: 'id', 11: 'invitationsEnabled', 12: 'isAbuser', 13: 'isAlive', 14: 'isBot', 15: 'isClientLoaded', 16: 'isConnected', 17: 'isHidden', 18: 'isLeaver', 19: 'isPreBattleOwner', 20: 'killedBuildingsCount', 21: 'maxHealth', 22: 'name', 23: 'playerMode', 24: 'preBattleIdOnStart', 25: 'preBattleSign', 26: 'prebattleId', 27: 'realm', 28: 'shipComponents', 29: 'shipId', 30: 'shipParamsId', 31: 'skinId', 32: 'teamId', 33: 'ttkStatus'}
 property_id_map = {value: key for key, value in id_property_map.items()}
 
 
