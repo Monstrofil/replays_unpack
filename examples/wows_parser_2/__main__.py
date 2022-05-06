ef61ae87d3b50f0db54d9cc405ffd9956e50e75c
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2022-05-06 14:57:02 +0300

    added new example that looks for exclusive replays

---
+++ examples/wows_parser_2/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding=utf-8
+import argparse
+import glob
+
+from replay_unpack.replay_reader import ReplayReader
+from player import ReplayPlayer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pattern', required=True)
+
+    args = parser.parse_args()
+    for path in glob.glob(args.pattern):
+        print("Checking", path)
+
+        reader = ReplayReader(path)
+        replay = reader.get_replay_data()
+
+        player = ReplayPlayer(replay.engine_data
+                               .get('clientVersionFromXml')
+                               .replace(' ', '')
+                               .split(','))
+        player.play(replay.decrypted_data, strict_mode=True)
---
+++ examples/wows_parser_2/player.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import os
+
+from replay_unpack.clients.wows.network.packets import (
+    Position
+)
+from replay_unpack.clients.wows.player import ReplayPlayer as WoWSReplayPlayer
+
+
+from replay_unpack.core import PrettyPrintObjectMixin
+
+EXPECTED_TYPE = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' \
+                b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' \
+                b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' \
+                b'\x80\xbf\x00\x00\x80\xbf\x00\x00\x80\xbf'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class UnknownPacket(PrettyPrintObjectMixin):
+
+    def __init__(self, stream):
+        assert stream.read() == EXPECTED_TYPE
+
+
+class ReplayPlayer(WoWSReplayPlayer):
+
+    def _get_packets_mapping(self):
+        return {
+            0x18: UnknownPacket,
+        }
+
+    def _process_packet(self, packet):
+
+        if isinstance(packet, Position):
+            print('Entity %s had position %s; new position is %s' % (
+                packet.entityId,
+                self._battle_controller.entities[packet.entityId].position,
+                packet.position
+            ))
+
+            self._battle_controller.entities[packet.entityId].position = packet.position
+            self._battle_controller.entities[packet.entityId].yaw = packet.yaw
+            self._battle_controller.entities[packet.entityId].pitch = packet.pitch
+            self._battle_controller.entities[packet.entityId].roll = packet.roll
+
+        super(ReplayPlayer, self)._process_packet(packet)
