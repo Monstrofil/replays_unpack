2b0adef70e4e1432df8688a6a7c8ead8d7a5bcf5
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2022-12-10 20:19:42 +0200

    Fix package build to include all files

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='World of Warships replays parser',
-    version='3.3.0',
+    version='3.3.3',
     description='Utility which loads .wowsreplay file, "plays" it '
                 'in memory and produces results in json format',
     author='Oleksandr Shyshatskyi',
     author_email='shalal545@gmail.com',
     license='MIT',
     packages=find_packages(include='replay_unpack.*'),
-    package_data={'': ['*.py', '**/scripts/**/*.def', '**/scripts/**/*.xml']},
+    package_data={'': ['*.py', '**/scripts/**/*.def', '**/scripts/**/**/*.def', '**/scripts/**/*.xml', '**/scripts/*.xml']},
     include_package_data=True,
     scripts=['replay_parser.py'],
 )
