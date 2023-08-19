02f32107eb48424340460ac750188d29378d3359
Oleksandr Shyshatskyi <shalal545@gmail.com>
2023-08-19 19:24:04 +0300

    remove accidently added text

--- replay_unpack/clients/wows/network/packets/BattleStats.py
+++ replay_unpack/clients/wows/network/packets/BattleStats.py
@@ -9,5 +9,3 @@ class BattleStats(PrettyPrintObjectMixin):
     def __init__(self, stream):
         self.payloadSize, = struct.unpack('i', stream.read(4))
         self.payload = stream.read(self.payloadSize)
-
-        print(self.payload)
